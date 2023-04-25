d6246175b461cf70f665e826d5381038b6731aae
Zim <obizimuzo@gmail.com>
2023-04-25 14:41:21 +0100

    filter

--- src/pages/job-boards/index.js
+++ src/pages/job-boards/index.js
@@ -1,11 +1,26 @@
 import { Search } from '@/components/forms/search'
 import { JbCards } from '@/components/cards/jbcards'
 import Styles from '@/pages/job-boards/Jobs.module.css'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { authContext } from '@/context/auth'
+import { FiAlertCircle } from 'react-icons/fi'
 
 export default function Jobs() {
     const { jobs } = useContext(authContext)
+    const [sort, setSort] = useState("")
+
+    const sortType = () => {
+        if(sort === "full-time") {
+           return jobs.filter((r) => r.type === "full-time")
+        }else if(sort === "part-time") {
+           return jobs.filter((r) => r.type === "part-time")
+        }else if(sort === "internship") {
+            return jobs.filter((r) => r.type === "internship")
+        }else {
+            return jobs
+        }
+    }
+    
     return (
         <section className={Styles.viewsJobs}>
             <header className={Styles.header_bg}>
@@ -18,21 +33,24 @@ export default function Jobs() {
                 <label className={Styles.labelTitle} htmlFor="filter">Filter by</label>
         <div className={Styles.checkboxes}>
             <div className={Styles.box}>
-                <input type="radio" id="internship" name="filter" value="internship" />
+                <input type="radio" id="internship" name="filter" value="internship" onChange={(e) => setSort(e.target.value)} />
                 <label htmlFor="internship">Internship</label>
             </div>
             <div className={Styles.box}>
-                <input type="radio" id="partTime" name="filter" value="partTime" />
+                <input type="radio" id="partTime" name="filter" value="part-time" onChange={(e) => setSort(e.target.value)} />
                 <label htmlFor="partTime">Part time</label>
             </div>
             <div className={Styles.box}>
-                <input type="radio" id="fulltime" name="filter" value="fulltime" />
+                <input type="radio" id="fulltime" name="filter" value="full-time" onChange={(e) => setSort(e.target.value)} />
                 <label htmlFor="fulltime">Full-time</label>
             </div>
         </div>
     </div>
-    {jobs.length === 0 && <div>The search did not match any jobs!!!</div>}
-    <JbCards posts={jobs} />
+    {sortType().length === 0 && <div className={Styles.empty}>
+        <h1>The search did not match any jobs!!!</h1>
+        <p><FiAlertCircle /></p>
+        </div>}
+    <JbCards posts={sortType()} />
    </div>
 </section>
     )
--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,14 +1,60 @@
+import { useContext, useEffect, useState } from "react";
 import { authContext } from "@/context/auth";
-import { useContext } from "react";
 import Styles from "@/pages/profile/profile.module.css";
+import { Input } from "@/components/forms/input";
+import { Button } from "@/components/forms/button";
+import { FiArrowLeft } from "react-icons/fi";
 
-export default function Profile() {
+const Profile = () => {
   const { user } = useContext(authContext);
+  console.log(user);
+  // const [formData, setFormData] = useState({
+  //   newName: user.name,
+  //   number: user.number,
+  //   address: user.address,
+  // });
+  // const { newName, number, address } = formData;
+
+  // const onChange = (e) => {
+  //   setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  // };
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    // console.log(formData);
+  }
 
   return (
     <section className={Styles.container}>
-      <h1>Name: {user.name}</h1>
-      <p>Email: {user.email}</p>
+      <FiArrowLeft />
+      <p>{user?.name ?? ""}</p>
+      <form className={Styles.formUpdate} onSubmit={onSubmit}>
+      <Input
+          // value={newName}
+          name="newName"
+          // click={onChange}
+          type="text"
+          placeholder="Enter Name"
+        />
+        {/* <p className={Styles.email}>Email: {user.email}</p> */}
+        <Input
+          // value={number}
+          name="number"
+          // click={onChange}
+          type="number"
+          placeholder="Phone Number"
+        />
+        <Input
+          // value={address}
+          name="address"
+          // click={onChange}
+          type="text"
+          placeholder="Address"
+        />
+        <Button type="submit" value="Update" />
+      </form>
     </section>
   );
 }
+
+export default Profile
